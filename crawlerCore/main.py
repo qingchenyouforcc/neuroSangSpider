a5282067abb8fbaf2507e645315a84b83beb4a8e
qingchenyouforcc <90140161+qingchenyouforcc@users.noreply.github.com>
2025-04-02 20:16:42 +0800

    feat: 更新爬取逻辑，加入多线程

--- crawlerCore/main.py
+++ crawlerCore/main.py
@@ -5,7 +5,7 @@ import threading
 from utils.fileManager import create_dir
 
 # UP主列表 和 爬取视频需包含词
-up_list = [351692111]
+# up_list = [351692111]
 up_list = [351692111, 1880487363, 22535350, 3546612622166788, 5971855, 483178955]
 words_set = ["合唱", "歌回"]
 
