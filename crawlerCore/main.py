8eb063ec9be396c70f589a459c5b34cf79501825
qingchenyouforcc <90140161+qingchenyouforcc@users.noreply.github.com>
2025-04-02 19:16:09 +0800

    fix: 修复同步url和title时出现的严重bug

--- crawlerCore/main.py
+++ crawlerCore/main.py
@@ -4,13 +4,20 @@ from crawlerCore.videosList import show_video_list
 up_list = [351692111, 1880487363, 22535350, 3546612622166788, 5971855, 483178955]
 words_set = ["合唱", "歌回"]
 
+videos_temp_list = []
 videos_list = []
+videos_num = 0
 
 for up in up_list:
     videos = show_video_list(up, words_set)
-    videos_list.append(videos)
+    videos_temp_list.append(videos)
+
+for videos in videos_temp_list:
+    for video in videos:
+        videos_list.append(video)
 
 for video in videos_list:
     print(video)
+    videos_num += 1
 
-print("爬取完成")
+print(f"爬取完成，共{videos_num}个视频")
