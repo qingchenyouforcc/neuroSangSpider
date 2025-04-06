917eec37c79d4fd2d8085ea34248dbee234640ea
qingchenyouforcc <90140161+qingchenyouforcc@users.noreply.github.com>
2025-04-06 15:54:37 +0800

    feat: 增加主程序，方便调用

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from crawlerCore.main import create_video_list_file
 from musicDownloader.main import download_main
 
 if __name__ == '__main__':
-    # create_video_list_file()
+    create_video_list_file()
     download_main()
