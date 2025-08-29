5e61b282cf31091a026e891b89a4909dba92a236
qingchenyouforcc <qingchenformc@outlook.com>
2025-08-29 18:06:37 +0800

    refactor(utils): 移除未使用的导入项
    移除了 src/utils/file.py 中未使用的配置导入项 cfg 和 MUSIC_DIR。这个改动提高了代码的可读性和维护性。

--- src/utils/file.py
+++ src/utils/file.py
@@ -9,7 +9,7 @@ from PyQt6.QtCore import Qt
 from qfluentwidgets import InfoBar, InfoBarPosition
 from tqdm import tqdm
 
-from src.config import FFMPEG_PATH, MUSIC_DIR, cfg, subprocess_options
+from src.config import FFMPEG_PATH, MUSIC_DIR, subprocess_options
 from src.app_context import app_context
 from src.bili_api.converters import url2bv
 
