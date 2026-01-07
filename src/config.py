5ac17f1bbd708fe0b2e987502db4388b60778438
qingchenyouforcc <qingchenformc@outlook.com>
2026-01-07 19:11:44 +0800

    chore(release): 更新版本号到 1.2.1
    - 更新 src/config.py 中的 VERSION 变量
    - 更新 pyproject.toml 中的项目版本
    - 更新 version_info.txt 中的文件版本信息
    - 更新 version_info.txt 中的产品版本信息

--- src/config.py
+++ src/config.py
@@ -233,7 +233,7 @@ else:
     setTheme(QtTheme.AUTO)
 
 
-VERSION = "1.2.0"
+VERSION = "1.2.1"
 
 USER_AGENT = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
