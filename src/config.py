2aa3c266464b72fa037e393fcac528a40ced1aab
qingchenyouforcc <qingchenformc@outlook.com>
2026-01-19 14:44:04 +0800

    feat(core): 更新版本号到1.3.0
    - 将构建版本从1.2.2更新到1.3.0
    - 将构建编号从436更新到437
    - 更新Git哈希值和构建时间戳
    - 在配置文件中同步版本号变更
    - 更新pyproject.toml中的项目版本
    - 更新锁定文件中的包版本信息
    - 更新版本信息文件中的文件版本和产品版本

--- src/config.py
+++ src/config.py
@@ -250,7 +250,7 @@ else:
     setTheme(QtTheme.AUTO)
 
 
-VERSION = "1.2.2"
+VERSION = "1.3.0"
 
 USER_AGENT = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
